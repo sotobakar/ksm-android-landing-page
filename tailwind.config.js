7e03a3c1325e3cfdc15ddfbd383cad998eff3b15
Patrick Santino <pet.santino@gmail.com>
2021-07-10 18:01:08 +0700

    Homepage + Project page not done
    Co-authored-by: 2010511138@mahasiswa.upnvj.ac.id <2010511138@mahasiswa.upnvj.ac.id>
    Co-authored-by: Rahmat Afriyanton <rahmatafriyanton@users.noreply.github.com>
    Co-authored-by: Aditya Yoga Adhiputra <adityayogoal33@gmail.com>

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#11772d',
+        secondary: '#fdb141',
+        tertiary: '#f4f4f4',
+      },
+      fontFamily : {
+        'viceCitySans' : ['Vice City Sans', 'sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
